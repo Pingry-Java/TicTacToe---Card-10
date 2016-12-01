da2c8a994ebe4971a9089e9434574393a710d5ef
Drew Beckmen <dbeckmen>
2016-12-01 08:34:13 -0500

    Outlined methods for API.

---
+++ TicTacToe.java
@@ -0,0 +1,44 @@
+import java.util.Scanner; 
+
+public class TicTacToe
+{
+	/**
+	 * This method initializes the board and returns that 2D array
+	 * @return 2D array filled with "spots" on the tic tac toe board
+	 */ 
+	 public static int[][] initializeBoard()
+	 {
+	 
+	 }
+	 
+	 /**
+	  * Prints the tic tac toe board in a nicely formatted grid
+	  * @param grid The board to be printed
+	  */
+	  public static void printGrid(int[][] grid)
+	  {
+	  
+	  }
+	  
+	  /**
+	   * Places the player's symbol in the 2D array board
+	   * @param grid The tic tac toe board
+	   * @param symbol The symbol of the player that is up
+	   * @return The updated tic tac toe board
+	   */
+	   public static int[][] placeSymbol(int[][] grid, String symbol)
+	   {
+	   
+	   }
+	   
+	   /**
+	    * Takes in the board and checks if there is a winner
+	    * @param board The tic tac toe board being searched
+	    * @return Boolean value true for win and false for no win
+	    */
+	    public static boolean checkWinner(int[][] board)
+	    {
+	    
+	    }
+
+}
