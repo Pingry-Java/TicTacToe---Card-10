3d85a4a29f07aa105b0f57e365d3f8b7a91de014
Miro Bergam <mbergam2019@pingry.org>
2016-12-01 08:44:54 -0500

    added printGrid

--- TicTacToe.java
+++ TicTacToe.java
@@ -6,19 +6,31 @@ public class TicTacToe
 	 * This method initializes the board and returns that 2D array
 	 * @return 2D array filled with "spots" on the tic tac toe board
 	 */ 
-	 public static int[][] initializeBoard()
-	 {
+	public static int[][] initializeBoard()
+	{
 	 
-	 }
+	}
 	 
 	 /**
 	  * Prints the tic tac toe board in a nicely formatted grid
 	  * @param grid The board to be printed
 	  */
-	  public static void printGrid(int[][] grid)
-	  {
-	  
-	  }
+	public static void printGrid(int[][] grid)
+	{
+		for (int row = 0; row < grid.length; row++)
+		{
+			for (int col = 0; col < grid[row].length; col++)
+				System.out.print(" –––––––");
+			System.out.println();
+			System.out.print("|");
+			for (int col = 0; col < grid[row].length; col++)
+				System.out.print(grid[row][col] + "\t |");
+			System.out.println();
+		}
+		for (int col = 0; col < grid[0].length; col++)
+			System.out.print(" –––––––");
+		System.out.println();
+	}
 	  
 	  /**
 	   * Places the player's symbol in the 2D array board
@@ -26,19 +38,19 @@ public class TicTacToe
 	   * @param symbol The symbol of the player that is up
 	   * @return The updated tic tac toe board
 	   */
-	   public static int[][] placeSymbol(int[][] grid, String symbol)
-	   {
+	public static int[][] placeSymbol(int[][] grid, String symbol)
+	{
 	   
-	   }
+	}
 	   
-	   /**
-	    * Takes in the board and checks if there is a winner
-	    * @param board The tic tac toe board being searched
-	    * @return Boolean value true for win and false for no win
-	    */
-	    public static boolean checkWinner(int[][] board)
-	    {
+	/**
+	* Takes in the board and checks if there is a winner
+	* @param board The tic tac toe board being searched
+	* @return Boolean value true for win and false for no win
+	*/
+	public static boolean checkWinner(int[][] board)
+	{
 	    
-	    }
+	}
 
 }
